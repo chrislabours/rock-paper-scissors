0f41f9799c53874e65de6a67b567609693343f39
chrislabours <chrislabours@gmail.com>
2021-03-15 17:00:32 +0800

    Add five rounds of gameplay

--- script.js
+++ script.js
@@ -35,5 +35,11 @@ function game() {
     return playRound(playerSelection, computerSelection);
 }
 
-console.log(game());
-console.log(score());
+function round(num) {
+    for (let i = 0; i < num; i++) {
+        console.log(game());
+        console.log(score());
+    }
+}
+
+round(5);
